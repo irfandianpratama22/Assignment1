cb6de37f52fc1fc2c66845837efdd92a74acedc8
Irfan Dian Pratama <irfandp122@gmail.com>
2023-08-29 12:19:27 +0700

    Initial commit

---
+++ app/src/main/java/com/hacktiv8/siklushidup/MainActivity.java
@@ -0,0 +1,67 @@
+package com.hacktiv8.siklushidup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "SIKLUS HIDUP ACT";
+    EditText komentarText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println(TAG + " On Create");
+
+        komentarText = (EditText) findViewById(R.id.komentar_Text);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,"On Start",Toast.LENGTH_SHORT).show();
+        System.out.println(TAG + " On Start");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "On Stop", Toast.LENGTH_SHORT).show();
+        System.out.println(TAG + " On Stop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "On Pause", Toast.LENGTH_SHORT).show();
+        System.out.println(TAG + " On Pause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "On Resume", Toast.LENGTH_SHORT).show();
+        System.out.println(TAG + " On Resume ");
+
+        // Ambil Data Yang Tersimpan
+        String dataTersimpan = this.getSharedPreferences("siklus hidup", Context.MODE_PRIVATE).getString("komentar", null);
+        komentarText.setText(dataTersimpan);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "On Destroy", Toast.LENGTH_SHORT).show();
+        System.out.println(TAG + " On Destroy  ");
+        // Simpan Data Komentar
+        SharedPreferences.Editor data = this.getSharedPreferences("siklus hidup", Context.MODE_PRIVATE).edit();
+        data.putString("komentar", komentarText.getText().toString());
+        data.apply();
+    }
+}
